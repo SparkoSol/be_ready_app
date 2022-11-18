7d420a9064f1f7687bcf389a003fd15d7d4664c7
osamasandhu <osamasandhu3636@gmail.com>
2022-11-18 17:56:19 +0500

    journey page

--- lib/src/base/assets.dart
+++ lib/src/base/assets.dart
@@ -5,4 +5,6 @@ class AppAssets {
   static const hiddenIcon = '$_path/hidden_icon.png';
   static const messageIcon = '$_path/message_icon.png';
   static const passwordIcon = '$_path/password_icon.png';
+  static const journeyHome = '$_path/journey.png';
+  static const playButton = '$_path/play.png';
 }
