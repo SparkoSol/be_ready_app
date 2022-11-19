84229a60d696b1f0b5dbed596067efaaee425120
Umer Saddiq <umersaddiq49@gmail.com>
2022-11-19 17:45:15 +0500

    Home Page UI

--- lib/src/components/home/home_page.dart
+++ lib/src/components/home/home_page.dart
@@ -5,12 +5,12 @@ import 'package:be_ready_app/src/components/home/drawer_widget.dart';
 import 'package:be_ready_app/src/components/main_menu/be_connected.dart';
 import 'package:be_ready_app/src/components/main_menu/daily_check_in_page.dart';
 import 'package:be_ready_app/src/components/main_menu/events.dart';
+import 'package:be_ready_app/src/components/main_menu/resource_page.dart';
 import 'package:be_ready_app/src/widgets/app_bar.dart';
 import 'package:be_ready_app/src/widgets/app_button_widget.dart';
 import 'package:be_ready_app/src/widgets/background_image_widget.dart';
 import 'package:be_ready_app/src/widgets/gradient_progress_indicator.dart';
 import 'package:be_ready_app/src/widgets/main_page_widget.dart';
-import 'package:be_ready_app/src/widgets/text.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -21,49 +21,159 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final padding = MediaQuery.of(context).padding;
     return Scaffold(
-        drawer: const AppDrawer(),
-        resizeToAvoidBottomInset: false,
-        extendBody: true,
-        extendBodyBehindAppBar: true,
-        appBar: AppBarWidget(),
-        body: BackgroundImageWidget(
-          child: Padding(
-            padding: EdgeInsets.only(
-                left: 20,
-                right: 20,
-                bottom: 20,
-                top: MediaQuery.of(context).viewPadding.top + 56),
-            child: CustomScrollView(
-              slivers: [
-                const SliverToBoxAdapter(
-                  child: GoalsPageTitle(text: 'Resources'),
-                ),
-                const SliverToBoxAdapter(
-                  child:
-                      GoalsPageDescription(text: 'Absorb. Digest. Integrate.'),
-                ),
-                SliverPadding(padding: EdgeInsets.only(bottom: 20)),
-                SliverGrid(
-                  delegate: SliverChildBuilderDelegate(
-                      (context, index) => MainMenuWidget(
-                            onPressed: () {
-                              AppNavigation.to(context, DailyCheckInPage());
-                            },
-                            text: 'Daily check-In',
-                            path: AppAssets.graphIcon,
-                          )),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 30,
-                    crossAxisSpacing: 10,
-                    // childAspectRatio: 0.8,
-                    // mainAxisExtent: 200,
-                  ),
-                )
-              ],
-            ),
+      drawer: const AppDrawer(),
+      resizeToAvoidBottomInset: false,
+      extendBody: true,
+      extendBodyBehindAppBar: true,
+      appBar: AppBarWidget(),
+      body: BackgroundImageWidget(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+            top: MediaQuery.of(context).viewPadding.top + 56,
           ),
-        ));
+          child: CustomScrollView(slivers: [
+            const SliverToBoxAdapter(
+              child: Text(
+                'Welcome, Laurie',
+                style: TextStyle(fontSize: 33, color: Colors.white),
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.only(top: 2, bottom: 28),
+              sliver: SliverToBoxAdapter(
+                child: Text(
+                  'We’re glad you’re here.',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: AppCourseButtonWidget(
+                title: 'Continue Coursework',
+                onTap: () {},
+                isShadowed: true,
+              ),
+            ),
+            SliverGrid(
+              delegate: SliverChildListDelegate([
+                MainMenuWidget(
+                  onPressed: () {
+                    AppNavigation.to(context, const DailyCheckInPage());
+                  },
+                  text: 'Daily check-In',
+                  path: AppAssets.graphIcon,
+                ),
+                MainMenuWidget(
+                  onPressed: () {
+                    AppNavigation.to(context, ResourcePage());
+                  },
+                  text: 'Resources',
+                  path: AppAssets.graphIcon,
+                ),
+                MainMenuWidget(
+                  onPressed: () {
+                    AppNavigation.to(context, const EventsPage());
+                  },
+                  text: 'Events',
+                  path: AppAssets.calenderIcon,
+                ),
+                MainMenuWidget(
+                  onPressed: () {
+                    AppNavigation.to(context, const BeConnected());
+                  },
+                  text: 'Be Connected',
+                  path: AppAssets.userIcon,
+                ),
+              ]),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 30,
+                crossAxisSpacing: 10,
+              ),
+            ),
+            const SliverPadding(padding: EdgeInsets.all(10)),
+            SliverToBoxAdapter(
+              child: Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        const Color(0xFFF0D781).withOpacity(0.25),
+                        const Color(0xFFF0D781).withOpacity(0),
+                      ],
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                  padding: const EdgeInsets.all(18),
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFF0D781).withOpacity(0.20),
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          const Color(0xFFF0D781),
+                          const Color(0xFFF0D781).withOpacity(0),
+                        ],
+                      ),
+                    ),
+                    child: GradientProgressIndicator(
+                      value: 50,
+                      radius: 62,
+                      duration: 2,
+                      strokeWidth: 6,
+                      gradientStops: const [0.1, 1.0],
+                      gradientColors: const [
+                        Color(0xFFE0DFA3),
+                        Color(0xFF7391D4),
+                      ],
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 26,
+                          horizontal: 18,
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: const LinearGradient(
+                            colors: AppColors.buttonGradient,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              offset: const Offset(0, 7),
+                              blurRadius: 9,
+                              spreadRadius: 0,
+                              color: Colors.black.withOpacity(0.25),
+                            ),
+                          ],
+                        ),
+                        child: const Text(
+                          'YOUR\nJOURNEY',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 19,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
     Scaffold(
       drawer: const AppDrawer(),
       resizeToAvoidBottomInset: false,
@@ -82,26 +192,6 @@ class HomePage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  'Welcome, Laurie',
-                  style: TextStyle(fontSize: 33, color: Colors.white),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 2, bottom: 28),
-                  child: Text(
-                    'We’re glad you’re here.',
-                    style: GoogleFonts.poppins(
-                      color: Colors.white.withOpacity(0.8),
-                      fontSize: 18,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                ),
-                AppCourseButtonWidget(
-                  title: 'Continue Coursework',
-                  onTap: () {},
-                  isShadowed: true,
-                ),
                 Padding(
                   padding: const EdgeInsets.only(top: 37, bottom: 35),
                   child: Row(
@@ -109,7 +199,7 @@ class HomePage extends StatelessWidget {
                     children: [
                       MainMenuWidget(
                         onPressed: () {
-                          AppNavigation.to(context, DailyCheckInPage());
+                          AppNavigation.to(context, const DailyCheckInPage());
                         },
                         text: 'Daily check-In',
                         path: AppAssets.graphIcon,
@@ -127,14 +217,14 @@ class HomePage extends StatelessWidget {
                   children: [
                     MainMenuWidget(
                       onPressed: () {
-                        AppNavigation.to(context, EventsPage());
+                        AppNavigation.to(context, const EventsPage());
                       },
                       text: 'Events',
                       path: AppAssets.calenderIcon,
                     ),
                     MainMenuWidget(
                       onPressed: () {
-                        AppNavigation.to(context, BeConnected());
+                        AppNavigation.to(context, const BeConnected());
                       },
                       text: 'Be Connected',
                       path: AppAssets.userIcon,
@@ -142,76 +232,6 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
                 const SizedBox(height: 15),
-                Center(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          const Color(0xFFF0D781).withOpacity(0.25),
-                          const Color(0xFFF0D781).withOpacity(0),
-                        ],
-                      ),
-                      shape: BoxShape.circle,
-                    ),
-                    padding: const EdgeInsets.all(18),
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFF0D781).withOpacity(0.20),
-                        shape: BoxShape.circle,
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            const Color(0xFFF0D781),
-                            const Color(0xFFF0D781).withOpacity(0),
-                          ],
-                        ),
-                      ),
-                      child: GradientProgressIndicator(
-                        value: 50,
-                        radius: 62,
-                        duration: 2,
-                        strokeWidth: 6,
-                        gradientStops: const [0.1, 1.0],
-                        gradientColors: const [
-                          Color(0xFFE0DFA3),
-                          Color(0xFF7391D4),
-                        ],
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 26,
-                            horizontal: 18,
-                          ),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            gradient: const LinearGradient(
-                              colors: AppColors.buttonGradient,
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                offset: const Offset(0, 7),
-                                blurRadius: 9,
-                                spreadRadius: 0,
-                                color: Colors.black.withOpacity(0.25),
-                              ),
-                            ],
-                          ),
-                          child: const Text(
-                            'YOUR\nJOURNEY',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 19,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
               ],
             ),
           ),
--- lib/src/widgets/main_page_widget.dart
+++ lib/src/widgets/main_page_widget.dart
@@ -14,96 +14,99 @@ class MainMenuWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(clipBehavior: Clip.none, children: [
-      Positioned(
-        top: 16,
-        left: 18,
-        right: 18,
-        child: Container(
-          height: 155,
-          width: 131,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(27),
-            gradient: LinearGradient(
-              end: Alignment.bottomRight,
-              begin: Alignment.centerLeft,
-              colors: [
-                const Color(0xFF916DDD).withOpacity(0.12),
-                const Color(0xFF512E73).withOpacity(0.12),
-                // Colors.yellow,
-                // Colors.pink,
-              ],
+    return GestureDetector(
+      onTap: onPressed,
+      child: Stack(clipBehavior: Clip.none, children: [
+        Positioned(
+          top: 16,
+          left: 18,
+          right: 18,
+          child: Container(
+            height: 155,
+            width: 131,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(27),
+              gradient: LinearGradient(
+                end: Alignment.bottomRight,
+                begin: Alignment.centerLeft,
+                colors: [
+                  const Color(0xFF916DDD).withOpacity(0.12),
+                  const Color(0xFF512E73).withOpacity(0.12),
+                  // Colors.yellow,
+                  // Colors.pink,
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      Positioned(
-        top: 8,
-        left: 8,
-        right: 8,
-        child: Container(
-          height: 155,
-          width: 131,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(27),
-            gradient: LinearGradient(
-              end: Alignment.bottomRight,
-              begin: Alignment.centerLeft,
-              colors: [
-                const Color(0xFF916DDD).withOpacity(0.4),
-                const Color(0xFF512E73).withOpacity(0.4),
-              ],
+        Positioned(
+          top: 8,
+          left: 8,
+          right: 8,
+          child: Container(
+            height: 155,
+            width: 131,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(27),
+              gradient: LinearGradient(
+                end: Alignment.bottomRight,
+                begin: Alignment.centerLeft,
+                colors: [
+                  const Color(0xFF916DDD).withOpacity(0.4),
+                  const Color(0xFF512E73).withOpacity(0.4),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      Container(
-        padding: const EdgeInsets.symmetric(vertical: 10),
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          borderRadius: BorderRadius.circular(25),
-          gradient: const LinearGradient(
-            stops: [0.5, 1],
-            end: Alignment.bottomRight,
-            begin: Alignment.centerLeft,
-            colors: [
-              Color(0xff6f55a0),
-              Color(0xFF4F2A98),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(25),
+            gradient: const LinearGradient(
+              stops: [0.5, 1],
+              end: Alignment.bottomRight,
+              begin: Alignment.centerLeft,
+              colors: [
+                Color(0xff6f55a0),
+                Color(0xFF4F2A98),
+              ],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: Image.asset(path, width: 70, height: 70),
+              ),
+              Container(
+                height: 1,
+                width: 150,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    stops: const [0.5, 1],
+                    colors: [
+                      Colors.white.withOpacity(0.1),
+                      Colors.white,
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                text,
+                style: GoogleFonts.poppins(
+                  fontSize: 13,
+                  color: Colors.white,
+                  letterSpacing: 1,
+                ),
+              ),
             ],
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15),
-              child: Image.asset(path, width: 70, height: 70),
-            ),
-            Container(
-              height: 1,
-              width: 150,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  stops: const [0.5, 1],
-                  colors: [
-                    Colors.white.withOpacity(0.1),
-                    Colors.white,
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              text,
-              style: GoogleFonts.poppins(
-                fontSize: 13,
-                color: Colors.white,
-                letterSpacing: 1,
-              ),
-            ),
-          ],
-        ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
