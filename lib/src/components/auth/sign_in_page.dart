a7e9143646ab30cd662456686b3a8d51d90c02e0
Umer Saddiq <umersaddiq49@gmail.com>
2022-11-19 15:35:06 +0500

    Navigation

--- lib/src/components/auth/sign_in_page.dart
+++ lib/src/components/auth/sign_in_page.dart
@@ -1,10 +1,12 @@
 import 'package:be_ready_app/src/base/assets.dart';
 import 'package:be_ready_app/src/base/nav.dart';
+import 'package:be_ready_app/src/components/auth/reset_password_page.dart';
 import 'package:be_ready_app/src/components/auth/sign_up_page.dart';
 import 'package:be_ready_app/src/components/auth/widget/auth_text_span_widget.dart';
 import 'package:be_ready_app/src/components/auth/widget/auth_title_widget.dart';
 import 'package:be_ready_app/src/components/auth/widget/or_widget.dart';
 import 'package:be_ready_app/src/components/auth/widget/social_auth_button.dart';
+import 'package:be_ready_app/src/components/home/home_page.dart';
 import 'package:be_ready_app/src/components/journey/journey_page.dart';
 import 'package:be_ready_app/src/widgets/app_button_widget.dart';
 import 'package:be_ready_app/src/widgets/app_text_field.dart';
@@ -70,7 +72,9 @@ class _SignInPageState extends State<SignInPage> {
                       style: TextButton.styleFrom(
                         foregroundColor: Colors.white,
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        AppNavigation.to(context, const ResetPasswordPage());
+                      },
                       child: Text(
                         'Forgot Password?',
                         style: GoogleFonts.poppins(),
@@ -81,12 +85,15 @@ class _SignInPageState extends State<SignInPage> {
                 AppButtonWidget(
                   onPressed: () {
                     FocusScope.of(context).unfocus();
+                    AppNavigation.navigateRemoveUntil(context, const HomePage());
                   },
-                 title: 'SIGN IN',
+                  title: 'SIGN IN',
                 ),
                 const OrWidget(),
                 SocialAuthButton(
-                  onTap: () {AppNavigation.to(context, const JourneyHomePage());},
+                  onTap: () {
+                    AppNavigation.to(context, const JourneyHomePage());
+                  },
                   platformImage: AppAssets.google,
                   platformName: 'Google',
                 ),
