27d76af87caad744f41f052ca5f8fd351471769b
Umer Saddiq <umersaddiq49@gmail.com>
2022-11-19 09:55:33 +0500

    Appbar widget

--- lib/src/components/auth/sign_in_page.dart
+++ lib/src/components/auth/sign_in_page.dart
@@ -6,6 +6,7 @@ import 'package:be_ready_app/src/components/auth/widget/auth_text_span_widget.da
 import 'package:be_ready_app/src/components/auth/widget/auth_title_widget.dart';
 import 'package:be_ready_app/src/components/auth/widget/or_widget.dart';
 import 'package:be_ready_app/src/components/auth/widget/social_auth_button.dart';
+import 'package:be_ready_app/src/components/home/home_page.dart';
 import 'package:be_ready_app/src/widgets/app_button_widget.dart';
 import 'package:be_ready_app/src/widgets/app_text_field.dart';
 import 'package:be_ready_app/src/widgets/background_image_widget.dart';
@@ -81,6 +82,7 @@ class _SignInPageState extends State<SignInPage> {
                 AppButtonWidget(
                   onPressed: () {
                     FocusScope.of(context).unfocus();
+                    AppNavigation.navigateRemoveUntil(context, HomePage());
                   },
                   child: const AuthButtonTitleWidget(title: 'SIGN IN'),
                 ),
