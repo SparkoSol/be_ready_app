f4cb5a24c5342c1c4af6e215409a11482e1e9b86
osamasandhu <osamasandhu3636@gmail.com>
2022-11-19 17:32:11 +0500

    resources page

---
+++ lib/src/components/main_menu/resource_page.dart
@@ -0,0 +1,48 @@
+import 'package:be_ready_app/src/widgets/app_bar.dart';
+import 'package:be_ready_app/src/widgets/background_image_widget.dart';
+import 'package:be_ready_app/src/widgets/resources_widget.dart';
+import 'package:be_ready_app/src/widgets/text.dart';
+import 'package:flutter/material.dart';
+
+class ResourcePage extends StatelessWidget {
+  const ResourcePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBarWidget(),
+        body: BackgroundImageWidget(
+          child: Padding(
+            padding: EdgeInsets.only(
+                left: 20,
+                right: 20,
+                bottom: 20,
+                top: MediaQuery.of(context).viewPadding.top + 56),
+            child: CustomScrollView(
+              slivers: [
+                const SliverToBoxAdapter(
+                  child: GoalsPageTitle(text: 'Resources'),
+                ),
+                const SliverToBoxAdapter(
+                  child:
+                      GoalsPageDescription(text: 'Absorb. Digest. Integrate.'),
+                ),
+                SliverPadding(padding: EdgeInsets.only(bottom: 20)),
+                SliverGrid(
+                  delegate: SliverChildBuilderDelegate((context, index) =>
+                      const ResourceWidget(quantity: 525, text: 'Articelss')),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: 0.8,
+                    // mainAxisExtent: 200,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/src/components/main_menu/stack_images.dart
@@ -0,0 +1,39 @@
+import 'package:be_ready_app/src/base/assets.dart';
+import 'package:flutter/material.dart';
+
+class StackImages extends StatelessWidget {
+  const StackImages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        imageStack(0, AppAssets.graphIcon),
+        imageStack(12.5, AppAssets.user),
+        imageStack(25, AppAssets.graphIcon),
+        imageStack(37.5, AppAssets.graphIcon),
+        imageStack(50, AppAssets.userIcon),
+      ],
+    );
+  }
+
+  Widget imageStack(double left, String url) {
+    return Padding(
+        padding: EdgeInsets.only(
+          left: left,
+        ),
+        child: Container(
+          width: 25,
+          height: 25,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(url),
+              fit: BoxFit.cover,
+            ),
+            color: const Color(0xffb6b6b6),
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.white, width: 1),
+          ),
+        ));
+  }
+}
