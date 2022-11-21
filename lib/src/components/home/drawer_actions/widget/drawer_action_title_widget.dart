0f4f4905de00190404c2a0bfc3038ee5085a97bf
Umer Saddiq <umersaddiq49@gmail.com>
2022-11-21 12:04:56 +0500

    drawer_action_title_widget added

--- lib/src/components/home/drawer_actions/widget/drawer_action_title_widget.dart
+++ lib/src/components/home/drawer_actions/widget/drawer_action_title_widget.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 class DrawerActionTitleWidget extends StatelessWidget {
-  const DrawerActionTitleWidget({Key? key, required this.title})
-      : super(key: key);
+  const DrawerActionTitleWidget({
+    Key? key,
+    required this.title,
+    this.bottomPadding,
+  }) : super(key: key);
 
   final String title;
+  final double? bottomPadding;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 20, bottom: 8),
+      padding: EdgeInsets.only(top: 20, bottom: bottomPadding ?? 35),
       child: Text(
         title,
         style: const TextStyle(
