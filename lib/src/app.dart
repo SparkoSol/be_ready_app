26340c8651d195304ed707ba233d30009acb331d
Umer Saddiq <umersaddiq49@gmail.com>
2022-11-21 10:41:14 +0500

    Bottom Navigation Bar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,4 @@
 import 'package:be_ready_app/src/components/auth/sign_in_page.dart';
-import 'package:be_ready_app/src/components/goals/activites_page.dart';
-import 'package:be_ready_app/src/components/home/home_page.dart';
-import 'package:be_ready_app/src/components/main_menu/be_connected.dart';
-import 'package:be_ready_app/src/components/main_menu/daily_check_in_page.dart';
-import 'package:be_ready_app/src/components/goals/explore.dart';
-import 'package:be_ready_app/src/components/main_menu/events.dart';
-import 'package:be_ready_app/src/components/main_menu/resource_page.dart';
 import 'package:flutter/material.dart';
 
 import 'base/theme.dart';
@@ -18,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Be Ready',
       theme: AppTheme.lightTheme,
-      home:  SignInPage(),
+      home: const SignInPage(),
     );
   }
 }
