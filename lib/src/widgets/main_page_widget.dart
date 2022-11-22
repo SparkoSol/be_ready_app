45fb0ccd4026a6d49f3d03a62d42f8d3dbf1aede
Umer Saddiq <umersaddiq49@gmail.com>
2022-11-22 13:00:46 +0500

    Homepage - Main page widget widget fixes

--- lib/src/widgets/main_page_widget.dart
+++ lib/src/widgets/main_page_widget.dart
@@ -16,97 +16,100 @@ class MainMenuWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPressed,
-      child: Stack(clipBehavior: Clip.none, children: [
-        Positioned(
-          top: 16,
-          left: 18,
-          right: 18,
-          child: Container(
-            height: 155,
-            width: 131,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(27),
-              gradient: LinearGradient(
-                end: Alignment.bottomRight,
-                begin: Alignment.centerLeft,
-                colors: [
-                  const Color(0xFF916DDD).withOpacity(0.12),
-                  const Color(0xFF512E73).withOpacity(0.12),
-                  // Colors.yellow,
-                  // Colors.pink,
-                ],
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(
+            top: 16,
+            left: 18,
+            right: 18,
+            child: Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 4.3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(27),
+                gradient: LinearGradient(
+                  end: Alignment.bottomRight,
+                  begin: Alignment.centerLeft,
+                  colors: [
+                    const Color(0xFF916DDD).withOpacity(0.12),
+                    const Color(0xFF512E73).withOpacity(0.12),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        Positioned(
-          top: 8,
-          left: 8,
-          right: 8,
-          child: Container(
-            height: 155,
-            width: 131,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(27),
-              gradient: LinearGradient(
-                end: Alignment.bottomRight,
-                begin: Alignment.centerLeft,
-                colors: [
-                  const Color(0xFF916DDD).withOpacity(0.4),
-                  const Color(0xFF512E73).withOpacity(0.4),
-                ],
+          Positioned(
+            top: 8,
+            left: 8,
+            right: 8,
+            child: Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 4.4,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(27),
+                gradient: LinearGradient(
+                  end: Alignment.bottomRight,
+                  begin: Alignment.centerLeft,
+                  colors: [
+                    const Color(0xFF916DDD).withOpacity(0.4),
+                    const Color(0xFF512E73).withOpacity(0.4),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.circular(25),
-            gradient: const LinearGradient(
-              stops: [0.5, 1],
-              end: Alignment.bottomRight,
-              begin: Alignment.centerLeft,
-              colors: [
-                Color(0xff6f55a0),
-                Color(0xFF4F2A98),
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height / 4.5,
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(25),
+              gradient: const LinearGradient(
+                stops: [0.5, 1],
+                end: Alignment.bottomRight,
+                begin: Alignment.centerLeft,
+                colors: [
+                  Color(0xff6f55a0),
+                  Color(0xFF4F2A98),
+                ],
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Image.asset(path, width: 70, height: 70),
+                ),
+                Container(
+                  height: 1,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      stops: const [0.5, 1],
+                      colors: [
+                        Colors.white.withOpacity(0.1),
+                        Colors.white,
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  text,
+                  style: GoogleFonts.poppins(
+                    fontSize: 13,
+                    color: Colors.white,
+                    letterSpacing: 1,
+                  ),
+                ),
               ],
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: Image.asset(path, width: 70, height: 70),
-              ),
-              Container(
-                height: 1,
-                width: 150,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    stops: const [0.5, 1],
-                    colors: [
-                      Colors.white.withOpacity(0.1),
-                      Colors.white,
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                text,
-                style: GoogleFonts.poppins(
-                  fontSize: 13,
-                  color: Colors.white,
-                  letterSpacing: 1,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
