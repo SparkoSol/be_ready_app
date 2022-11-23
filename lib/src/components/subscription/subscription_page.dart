6083325c838e8abc0b4560263389f50e275178b2
Osama Sandhu <osamasandhu3636@gmail.com>
2022-11-23 17:13:24 +0500

    updated code

--- lib/src/components/subscription/subscription_page.dart
+++ lib/src/components/subscription/subscription_page.dart
@@ -20,145 +20,141 @@ class _SubscriptionPageState extends State<SubscriptionPage> {
       extendBodyBehindAppBar: true,
       appBar: AppBarWidget(),
       body: BackgroundImageWidget(
+        child: Padding(
+          padding:
+              EdgeInsets.only(top: MediaQuery.of(context).padding.top + 56),
           child: SingleChildScrollView(
-        padding:
-            EdgeInsets.only(top: MediaQuery.of(context).viewPadding.top + 60),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15),
-              child: Image.asset(AppAssets.temple, width: 100, height: 100),
-            ),
-            const SizedBox(
-              height: 45,
-            ),
-            Text(
-              'Exclusive Content',
-              style: GoogleFonts.oswald(
-                fontSize: 30,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Access exclusive content here',
-              style: GoogleFonts.poppins(
-                fontSize: 13,
-                color: Colors.white60,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            padding: const EdgeInsets.only(left: 52, right: 45),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 45),
+                  child: Image.asset(AppAssets.temple, width: 100, height: 100),
+                ),
+                Text(
+                  'Exclusive Content',
+                  style: GoogleFonts.oswald(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 21,
+                ),
+                Text(
+                  'Access exclusive content here',
+                  style: GoogleFonts.poppins(
+                    fontSize: 13,
+                    color: Colors.white60,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 46),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 10,
+                        width: 10,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFF6272E4),
+                              Color(0xFFF09793),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Container(
+                        height: 10,
+                        width: 10,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.white30),
+                      ),
+                    ],
+                  ),
+                ),
                 Container(
-                  height: 10,
-                  width: 10,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xFF6272E4),
-                        Color(0xFFF09793),
-                      ],
+                  width: double.infinity,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 26, horizontal: 2),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                    shape: BoxShape.rectangle,
+                    border:
+                        Border.all(color: const Color(0xFF444A88), width: 0.5),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        '7 days free, then \$59.99/year',
+                        style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        'only  \$0.16/day',
+                        style: GoogleFonts.poppins(
+                          fontSize: 10,
+                          color: Colors.white54,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(vertical: 36),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xFFD2876F),
+                          Color(0xFF523072),
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      'Try Free & Subscribe',
+                      style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
                     ),
                   ),
                 ),
                 const SizedBox(
-                  width: 8,
+                  height: 60,
                 ),
-                Container(
-                  height: 10,
-                  width: 10,
-                  decoration: const BoxDecoration(
-                      shape: BoxShape.circle, color: Colors.white30),
+                GestureDetector(
+                  onTap: () =>
+                      AppNavigation.to(context, const SubscriptionOffersPage()),
+                  child: Text(
+                    'View other plans',
+                    style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      color: Colors.white54,
+                    ),
+                  ),
                 ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 25, right: 25, top: 50),
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 45, vertical: 20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.transparent,
-                  shape: BoxShape.rectangle,
-                  border:
-                      Border.all(color: const Color(0xFF444A88), width: 0.5),
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      '7 days free, then \$59.99/year',
-                      style: GoogleFonts.poppins(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'only  \$0.16/day',
-                      style: GoogleFonts.poppins(
-                        fontSize: 10,
-                        color: Colors.white54,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 30),
-                padding: const EdgeInsets.fromLTRB(79, 36, 79, 30),
-                decoration: const BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  gradient: LinearGradient(
-                    colors: [
-                      Color(0xFFD2876F),
-                      Color(0xFF523072),
-                    ],
-                  ),
-                ),
-                child: Text(
-                  'Try Free & Subscribe',
-                  style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            GestureDetector(
-              onTap: () =>
-                  AppNavigation.to(context, const SubscriptionOffersPage()),
-              child: Text(
-                'View other plans',
-                style: GoogleFonts.poppins(
-                  fontSize: 14,
-                  color: Colors.white54,
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
